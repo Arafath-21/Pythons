9c407fca9c6a250af4ed17b9c47fe0b14de08f44
Arafath A <arafathyas21@gmail.com>
2024-10-21 18:00:36 +0530

    first day

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('hello World')
+import random
+
+def get_choices():
+    player_choice = input('Enter a choice(rock,paper,scissors) : ')
+    options = ["rock","paper","scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player":player_choice,"computer":computer_choice}
+    return choices
+
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    
+    if player == computer:
+        return "It's a tie!"
+    
+    # If player chooses rock
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    
+    # If player chooses paper
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cut paper! You lose."
+    
+    # If player chooses scissors
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cut paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."
+    
+    else:
+        return "Invalid choice. Please choose 'rock', 'paper', or 'scissors'."
+
+choices = get_choices()
+result = check_win(choices["player"],choices["computer"]);
+print(result)
---
+++ study.py
@@ -0,0 +1,51 @@
+# variables
+'''
+player_choice = 'rock'
+computer_choice = 'paper'
+
+'''
+
+# function
+'''
+function is a set of code which runs only on when it' called
+'''
+
+def first_function():
+    first_variable = 'rock'
+    second_variable = 'paper'
+
+    return first_variable
+
+result = first_function()
+print(result)
+
+# Dictionaries
+'''
+Dictionaries in python stores a data in which the data will be in key value pairs
+ex: dict = {"name":"yas","color":"blue"}
+
+'''
+
+# input function
+'''
+the input function gets input from the user
+'''
+
+# Libraries
+'''
+Python libraries are sets of code that provide predefined code structures, allowing us to avoid coding from scratch.
+'''
+
+# Lists
+'''
+Lists in Python are used to store multiple items in a single variable.
+Example: food = ["pizza", "carrots", "eggs"]
+'''
+
+# Function Arguments
+'''
+Function arguments in Python are values passed to the function within parentheses. 
+In Python, arguments are passed by "object reference," which means that mutable objects 
+(like lists or dictionaries) can be modified within the function, while immutable objects 
+(like integers, strings, or tuples) cannot affect the original value.
+'''
